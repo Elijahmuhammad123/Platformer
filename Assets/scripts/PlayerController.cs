55875f30463f5ef2e6c084ee8d77c4ad6b2dfb85
Elijah Muhammad <831598@mystma.org>
2021-01-25 13:22:40 -0600

    grounded jump

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,22 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     public float jumpPower;
     public float moveSpeed;
+    public float jumpDetectOffset;
+    public LayerMask jumpableobjects;
     Rigidbody2D rb;
     SpriteRenderer sr;
     Vector3 startPos;
+    BoxCollider2D bc;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         sr = gameObject.GetComponent<SpriteRenderer>();
+        bc = gameObject.GetComponent<BoxCollider2D>();
         startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(transform.up * jumpPower);
         }
@@ -65,5 +69,11 @@ public class PlayerController : MonoBehaviour
             transform.SetParent(null);
     }
        
-    
+    private bool IsGrounded()
+    {
+        RaycastHit2D RaycastHit = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size,0F, Vector2.down, jumpDetectOffset, jumpableobjects);
+            Color raycolor = Color.red;
+        Debug.DrawRay(bc.bounds.center - new Vector3(bc.bounds.extents.x, bc.bounds.extents.y + jumpDetectOffset), Vector2.right * bc.bounds.size.x, raycolor);
+        return RaycastHit.collider != null;
+    }
 }
